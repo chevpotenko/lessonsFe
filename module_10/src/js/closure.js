f761429867a2a6da744d76b143f3e515447839ff
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2016-08-29 10:25:23 +0300

    v.1.2

---
+++ module_10/src/js/closure.js
@@ -0,0 +1,46 @@
+$(function(){
+	/*task #1----------------------------------------*/
+	var counter = (function counter(){
+		var val = 0;
+		return {
+			count: function(){
+				return ++val;
+			}
+		}
+	})();
+	
+	$('.first').on('click', function(e){
+		e.preventDefault();
+		$('.count').html(counter.count());
+	});
+	
+	
+	/*task #3----------------------------------------*/
+	var links = $('.links')
+	for (var i = 0; i < links.length; i++) {
+		links[i].onclick = (function(i) {
+			return function(e){
+				e.preventDefault();
+				console.log(i);
+			}
+		})(i);
+	}
+})
+
+/*task #2----------------------------------------*/
+
+	var HelloPage = {
+	name: 'GoIT',
+	init: function() {
+		console.log('Hello, ' + this.name);
+	}
+}
+
+Function.prototype.bind = function (scope) {
+	var fn = this;
+	return function () {
+		return fn.apply(scope);
+	};
+}
+
+window.onload = HelloPage.init.bind(HelloPage);
