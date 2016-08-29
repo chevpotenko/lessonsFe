cc0ada2435ffd66236ad513ea9d6bcdf680e6841
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2016-08-29 10:30:05 +0300

    v.1.4

--- module_10/src/js/closure.js
+++ module_10/src/js/closure.js
@@ -29,7 +29,7 @@ $(function(){
 
 /*task #2----------------------------------------*/
 
-	var HelloPage = {
+var HelloPage = {
 	name: 'GoIT',
 	init: function() {
 		console.log('Hello, ' + this.name);
